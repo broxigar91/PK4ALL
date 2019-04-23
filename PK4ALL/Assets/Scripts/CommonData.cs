39c40ea32b5da46da9e52dad79ee4c455f12ca9e
broxigar91 <ichigobiar@gmail.com>
2019-04-23 21:38:54 +0200

    Development
    #5 Progress with monster (parent) class.
    All necessary data are on it

---
+++ PK4ALL/Assets/Scripts/CommonData.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommonData {
+
+    public enum Type
+    {
+        FIRE,
+        WATER
+    }
+
+    public struct BaseStats
+    {
+        byte hp;
+        byte atk;
+        byte def;
+        byte speed;
+        byte spatk;
+        byte spdef;
+    }
+
+    public enum GenderRate
+    {
+        ALWAYSMALE,
+        FEMALEONEEIGHTH,
+        FEMALE25PERCENT,
+        FEMALE50PERCENT,
+        FEMALE75PERCENT,
+        FEMALESEVENEIGHTHS,
+        ALWAYSFEMALE,
+        GENDERLESS
+    }
+    
+    public enum Gender
+    {
+        MALE,
+        FEMALE, 
+        GENDERLESS
+    }   
+    
+    public enum GrowthRate
+    {
+        FAST,
+        MEDIUM,
+        SLOW,
+        PARABOLIC,
+        ERRATIC,
+        FLUCTUATING
+    }
+
+    public struct EV
+    {
+        int hp;
+        int atk;
+        int def;
+        int speed;
+        int spatk;
+        int spdef;
+    }
+
+    public enum EggGroup
+    {
+        MONSTER,
+        WATER1, //sea creatures
+        BUG,
+        FLYING,
+        FIELD,
+        FAIRY,
+        GRASS,
+        HUMANLIKE,
+        WATER3, //shellfish
+        MINERAL,
+        AMORPHOUS,
+        WATER2, //fish
+        DITTO,
+        DRAGON,
+        UNDISCOVERED //cannot put eggs
+    }
+
+    public enum Color
+    {
+        BLACK,
+        BLUE,
+        BROWN,
+        GRAY,
+        GREEN,
+        PINK,
+        PURPLE,
+        RED,
+        WHITE,
+        YELLOW
+    }
+
+    public enum Shape
+    {
+        ONLYAHEAD,
+        SERPENTLIKE,
+        FISH,
+        HEADANDARMS,
+        HEADANDBASE,
+        BIPEDALWITHTAIL,
+        HEADANDLEGS,
+        QUADRUPED,
+        HASTWOWINGS,
+        TENTACLESORMULTIPLELEGS,
+        MULTIPLEFUSEDBODIES,
+        HUMANOID,
+        WINGEDINSECTOID,
+        INSECTOID
+    }
+
+    public enum Habitat
+    {
+        CAVE,
+        FOREST,
+        GRASSLAND,
+        MOUNTAIN,
+        RARE, //THIS OPTION CAN BE TAKEN AS ""UNKNOWN""
+        ROUGHTERRAIN,
+        SEA,
+        URBAN,
+        WATERSEDGE
+    }
+}
---
+++ PK4ALL/Assets/Scripts/Monster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CommonData;
+
+//Note when you read monster it mostly means pokemon, but due to the generalism of the code/game it will be called like this in case other collect monster game is done with this code
+
+public class Monster
+{
+    int id; //national dex number
+    string name;
+    string internalName;
+    Type type1;
+    Type type2;
+    BaseStats baseStats;
+    GenderRate genderRate;
+    GrowthRate growthRate;
+    ushort baseExp; //exp gained from defeat this kind of monster
+    EV effortPoints; //ev's gained from defeating a monster of the species
+    byte rareness; //	The catch rate of the species.The higher the number, the more likely a capture (0 means it cannot be caught by anything except a Master Ball).
+    byte happiness; //The amount of happiness a newly caught Pokémon of the species will have. Is typically 70. 
+    Dictionary<int, int> moves; //moves that all monsters of the species can learn. It's a dictionary with key being the id of the move and value being the lvl at witch the move is learned (0 means the move can only be learned when a monster evolves).
+    EggGroup eggGroup;
+    int stepsToHatch; //number of steps required to open the egg
+    #region USED_FOR_DEX_SEARCH
+    float height;
+    float weight;
+    CommonData.Color color; 
+    Shape shape;
+    string kind;
+    int dexEntry;
+    #endregion
+
+    Vector3Int Abilities;   //The int of the vector are references to the monster abilities
+                            //a vector3Int is choosed because monsters only can have 3 Abilities maximum (2 + hidden one) so instead of a list we use a Vector3 which has a faster acces to its values. 
+    List<int> eggMoves; //only for unevolved species
+}
