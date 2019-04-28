d8239aa79817607d4d90620d43a06873ecad18b7
broxigar91 <ichigobiar@gmail.com>
2019-04-28 16:12:51 +0200

    Create PokemonMove.cs
    Class for the pokemon instance

---
+++ PK4ALL/Assets/Scripts/PokemonMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class PokemonMove
+{
+    public Move move;
+    public byte currentPP;
+    byte ppUpUsed = 0; //max 3
+
+    public bool PpUp(bool maxPP)
+    {
+
+        if (ppUpUsed < 3)
+        {
+            byte upgradesAvaliable = (byte)(3 - ppUpUsed);
+
+            ppUpUsed += upgradesAvaliable; 
+            move.totalPP += (byte)(move.totalPP * (0.2 * upgradesAvaliable));
+            return true;
+        }
+
+        return false;
+    }
+    
+}
