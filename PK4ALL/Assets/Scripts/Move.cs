234416ce67a8598b3b971553292ee464b058dc38
broxigar91 <ichigobiar@gmail.com>
2019-04-28 16:11:49 +0200

    Develop
    Created the class Move.cs #5

---
+++ PK4ALL/Assets/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CommonData;
+
+[System.Serializable]
+public class Move
+{
+    public int id;
+    string internalName;
+    public string displayName;
+    public MoveCode moveCode;
+    public int basePower;
+    public Type type;
+    public DamageCategory damageCategory;
+    public byte accuracy;
+    public byte totalPP;
+    //additional effect chance to be done
+    sbyte priority; //from -6 to 6 range
+    //flags to be done
+    string description;
+}
