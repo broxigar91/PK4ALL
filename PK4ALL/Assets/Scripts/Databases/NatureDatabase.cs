34c183a6dac2abf8cd40bb3117a89758d58bdd27
broxigar91 <ichigobiar@gmail.com>
2019-05-02 19:58:36 +0200

    Develop
    Created file for natures database #10

---
+++ PK4ALL/Assets/Scripts/Databases/NatureDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CommonData;
+
+public class NatureDatabase : MonoBehaviour
+{
+    public List<Nature> natureDB;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
