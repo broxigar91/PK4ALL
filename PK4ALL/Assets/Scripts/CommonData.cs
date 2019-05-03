8d55c87196fa3f9ea622530bb0c340cdb4016243
broxigar91 <ichigobiar@gmail.com>
2019-05-03 19:10:05 +0200

    Develop
    #5 Added natures to CommonData
    #10 Started JSON to store natures

--- PK4ALL/Assets/Scripts/CommonData.cs
+++ PK4ALL/Assets/Scripts/CommonData.cs
@@ -305,7 +305,29 @@ namespace CommonData {
     public enum Natures
     {
         ADAMANT,
+        BASHFUL,
         BOLD,
+        BRAVE,
+        CALM,
+        CAREFUL,
+        DOCILE,
+        GENTLE,
+        HARDY,
+        HASTY,
+        IMPISH,
+        JOLLY,
+        LAX,
+        LONELY,
+        MILD,
+        MODEST,
+        NAIVE,
+        NAUGHTY,
+        QUIET,
+        QUIRKY,
+        RASH,
+        RELAXED,
+        SASSY,
+        SERIOUS,
         TIMID
     }
 
--- PK4ALL/Assets/Scripts/Databases/NatureDatabase.cs
+++ PK4ALL/Assets/Scripts/Databases/NatureDatabase.cs
@@ -2,21 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using CommonData;
+using System;
+using System.IO;
 
-public class NatureDatabase : MonoBehaviour
+[Serializable]
+public struct NatureDB
 {
     public List<Nature> natureDB;
+}
 
 
+
+public class NatureDatabaseController : MonoBehaviour
+{
+
+    public NatureDB db;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        string datos = File.ReadAllText(Application.dataPath + "/Resources/itemdatabase.json"); //establezco la ruta donde se encuentra el fichero json
+        db = JsonUtility.FromJson<NatureDB>(datos);
     }
 
-    // Update is called once per frame
-    void Update()
+    public Nature getNature(Natures nat)
     {
-        
+        return db.natureDB.Find(x => x.nature == nat);
     }
 }
